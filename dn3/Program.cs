65ae4fe5e62ef3005a214e41cf47d390c7c14e8e
David Gardiner <david@gardiner.net.au>
2019-09-24 19:09:59 +0930

    First cut

---
+++ dn3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using static System.Console;
+
+namespace dn3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello .NET Conf !"));
+
+            WriteLine($"Environment.OSVersion: {Environment.OSVersion}");
+            WriteLine($"Environment.Version: {Environment.Version}");
+            WriteLine($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
+            WriteLine($"CoreCLR Build: {((AssemblyInformationalVersionAttribute[])typeof(object).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
+            WriteLine($"CoreFX Build: {((AssemblyInformationalVersionAttribute[])typeof(Uri).Assembly.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute),false))[0].InformationalVersion.Split('+')[0]}");
+        }
+    }
+}
